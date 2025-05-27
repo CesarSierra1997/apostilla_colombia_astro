09ab5ceef18fe0cb449e309eef05c19fd8d0b8a6
Cesar_Sierra <cesarmallungo@gmail.com>
2025-05-27 01:06:17 -0500

    Deploy en cpnel - se agrega fichero cpanel.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,14 @@ import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap"; // 
 
 export default defineConfig({
+  base: "/v2/",
   integrations: [
     tailwind(),
     react(),
     robotsTxt(),
     sitemap() // 
   ],
-  site: 'https://apostillacolombia.com.co', // 
+  site: 'https://apostillacolombia.com.co/v2', // 
 
   vite: {
     resolve: {
