a97446143177834e58b2b990dcbc2534d91e2f1e
Cesar_Sierra <cesarmallungo@gmail.com>
2025-08-06 19:20:46 -0500

    Se agrega configuracion para animacion del componente quiente somos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      keyframes: {
+        scrollX: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+      animation: {
+        'scroll-x': 'scrollX 20s linear infinite',
+      },
+    },
+  },
+  plugins: [],
 }
